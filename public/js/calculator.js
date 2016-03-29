4994c2713c2bed31479f58079e4c290b4b8dc6cb
nicolette <nicolette.williams8@icloud.com>
2016-03-29 09:24:28 -0500

    calculator

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,55 +1,29 @@
-(function() {
-  "use strict";
-  // define input fields and buttons
-    var leftOperand = document.getElementById('leftOperand');
-    var operator = document.getElementById('operator');
-    var rightOperand = document.getElementById('rightOperand');
-    var buttons = document.getElementsByTagName('button');
+var box= document.getElementById('display');
 
-    var doTheMathDude = function(operator){
-        if(operator == '+'){
-            var result =  parseInt(leftOperand.value) + parseInt(rightOperand.value);
-        }else if(operator == '-'){
-            var result =  parseInt(leftOperand.value) - parseInt(rightOperand.value);
-        }else if(operator == '*'){
-            var result =  parseInt(leftOperand.value) * parseInt(rightOperand.value);
-        }else if(operator == '/'){
-            var result =  parseInt(leftOperand.value) / parseInt(rightOperand.value);
-        }
-    leftOperand.value = result;
-    rightOperand.value = '';    
-    }
-    
-    var beforeOperator = true;
-    var result;
-    var displayClickedButton = function(){
-      if(this.value == 'C'){
-        leftOperand.value = '';
-        operator.value = '';
-        rightOperand.value = '';
-        beforeOperator = true;
-      }     
-      if(beforeOperator){
-        if(this.value % 1 == 0){
-                leftOperand.value += this.value;
-            }else{
-                if(this.value == '+' || this.value == '-' || this.value == '*' || this.value == '/'){
-                    operator.value += this.value;
-                beforeOperator = false;
-                }
-            }
-        }else{
-            if(this.value % 1 == 0){
-                rightOperand.value += this.value;
-          }else if(this.value == '='){
-            doTheMathDude(operator.value);
-            operator.value ='';
-            beforeOperator = true;
-            }
-      }
-    }
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', displayClickedButton, false);
-  }
+function addToScreen(x) {
+    box.value += x;
 
-})();
+    if(x == 'c'){
+        box.value = '';
+    }
+
+}
+
+function answer() {
+    x = box.value;
+    x = eval(x);
+    box.value = x;
+}
+
+function backspace() {
+    var number = box.value;
+    var length = number.length-1;
+    var newNumber = number.substring(0 , length);
+    box.value = newNumber;
+}
+
+function power(y) {
+    x = box.value;
+    x = Math.pow(x , y);
+    box.value = x;
+}
